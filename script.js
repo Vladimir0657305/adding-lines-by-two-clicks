3d39bb8d0b8339d8a4142d3a0dcdc0d4f871df0a
Vladimir0657305 <vv0657305@gmail.com>
2022-10-19 15:44:10 +0300

    changes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let xCollision = '';
 let yCollision = '';
 let points = [];
+let outx = '';
+let outy = '';
 
 function Painting() {
 
@@ -166,45 +168,148 @@ function get_line_intersection(p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, p3_x, p3_y)
 }
 
 
+function animate() {
 
+    requestAnimationFrame(animate);
+    collapse();
+
+}
+
+
+
+let i = 0;
 // Collapse animation
-// function collapse(currentTime) {
-//     canvas = document.getElementById("canvas");
-//     context = canvas.getContext('2d');
+function collapse() {
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext('2d');
     
-//     let a = '';
-//     let b = '';
+    var time = new Date().getTime() * 0.002;
+    var x = Math.sin(time) * 96 + 128;
+    var y = Math.cos(time * 0.9) * 96 + 128;
+
+
+    let a = '';
+    let b = '';
     
 
-//         for (let i = 0; i < points.length; i++)  {
-//         a = (points[i][0] + points[i][2]) / 2;
-//         b = (points[i][1] + points[i][3]) / 2;
-//         context.beginPath();
-//         context.moveTo(points[i][0], points[i][1]);
-//         context.lineWidth = 3;
-//         context.lineTo(a, b);
-//         context.moveTo(points[i][2], points[i][3]);
-//         context.lineWidth = 3;
-//         context.lineTo(a, b);
-//         context.strokeStyle = "#ffffff";
-//         context.fillStyle = "#ffffff";
-//         context.fill();
-//         context.stroke();
-//         };
+        for (let i = 0; i < points.length; i++)  {
 
-//     requestAnimationFrame(function () {
-//                     animate(canvas, context);
-//                 });
+            // a = (points[i][0] + points[i][2]) / 2;
+            // b = (points[i][1] + points[i][3]) / 2;
+            console.log('==>', points[i]);
+            // drawLineTo(points[i][2], points[i][3], points[i][0], points[i][1],   outx, outy);
+
+            let deltaX = Math.abs(points[i][2] - points[i][0]),
+                deltaY = Math.abs(points[i][3] - points[i][1]),
+                signX = points[i][0] < points[i][2] ? 1 : -1,
+                signY = points[i][1] < points[i][3] ? 1 : -1,
+                error = deltaX - deltaY;
+            // while (points[i][0] != points[i][2] || points[i][1] != points[i][3]) {
+            while (points[i][0] != points[i][2] || points[i][1] != points[i][3]) {
+                // console.log('points[i][0], points[i][1], points[i][2], points[i][3]', points[i][0], points[i][1], points[i][2], points[i][3]);
+                let error2 = error * 2;
+                if (error2 > -deltaY) {
+                    error -= deltaY;
+                    points[i][0] += signX;
+                    // points[i][2] -= signX;
+                }
+                if (error2 < deltaX) {
+                    error += deltaX;
+                    points[i][1] += signY;
+                    // points[i][3] -= signY;
+                }
+                console.log('=!=!==>', points[i][0], points[i][1], points[i][2], points[i][3], error, error2)
+                // break;
+                context.beginPath();
+                context.moveTo(points[i][0], points[i][1]);
+                context.lineWidth = 3;
+                context.lineTo(points[i][2], points[i][3]);
+                context.moveTo(points[i][2], points[i][3]);
+                context.lineWidth = 3;
+                context.lineTo(points[i][0], points[i][1]);
+                context.strokeStyle = "#ffffff";
+                context.fillStyle = "#ffffff";
+                context.fill();
+                context.stroke();
+            }
+
+
+            
+
+
+
+
+            // console.log('!!!!!!!!!!!!!!', points[i][0], points[i][1], points[i][2], points[i][3]);
+            // context.beginPath();
+            // context.moveTo(points[i][0], points[i][1]);
+            // context.lineWidth = 3;
+            // context.lineTo(a, b);
+            // context.moveTo(points[i][2], points[i][3]);
+            // context.lineWidth = 3;
+            // context.lineTo(a, b);
+            // context.strokeStyle = "#ffffff";
+            // context.fillStyle = "#ffffff";
+            // context.fill();
+            // context.stroke();
+            // i++
+        }  
+        points = [];
+        requestAnimationFrame(function () {
+                    collapse();
+                }, 60);
+    };
+
     
     
-    // points = [];
-    // temp += 1;
     
+    
+
+    
+    
+
+document.querySelector('.coor').onclick = collapse;
+
+
+
+
+
+// function drawLineTo(x1, y1, x2, y2, step) {
+//     let deltaX = Math.abs(x2 - x1),
+//         deltaY = Math.abs(y2 - y1),
+//         signX = x1 < x2 ? 1 : -1,
+//         signY = y1 < y2 ? 1 : -1,
+//         error = deltaX - deltaY;
+//     while (x1 != x2 || y1 != y2) {
+//         outx = x1;
+//         outy = y1;
+//         console.log(x1, y1, x2, y2);
+//         let error2 = error * 2;
+//         if (error2 > -deltaY) {
+//             error -= deltaY;
+//             x1 += signX;
+//         }
+//         if (error2 < deltaX) {
+//             error += deltaX;
+//             y1 += signY;
+//         }
+//     }
+//     return (x1, y1, x2, y2);
 // }
-// document.querySelector('.coor').onclick = animate;
 
 
 
+
+// function step(p0_x, p0_y, p1_x, p1_y, stepx, stepy) {
+    // var x = 50, y = 50, x2 = 20, y2 = -10;
+    // let time = Math.max(Math.abs(p0_x - p1_x), Math.abs(p0_y - p1_y));
+    // for (var i = 0; i <= time; i++) {
+    //     let delta = i / time;
+    //     let stepx = delta * (p1_x - p0_x) + p0_x;
+    //     let stepy = delta * (p1_y - p0_y) + p0_y;
+        // console.log(stepx,stepy)
+//         document.write([Math.round(stepx), Math.round(stepy)] + "<br>")
+//     }
+// };
 
 
 
